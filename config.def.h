39fe62b39d731f9a011d06a6570584c6d8985b84
crlxs <belmontecarles@gmail.com>
2024-05-19 16:26:01 +0200

    Set border px size and updated README

--- config.def.h
+++ config.def.h
@@ -25,4 +25,4 @@ static unsigned int lines      = 10;
 static const char worddelimiters[] = " ";
 
 /* Size of the window border */
-static unsigned int border_width = 0;
+static unsigned int border_width = 2;
